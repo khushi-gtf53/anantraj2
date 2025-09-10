8ec5341f00f4eb5898b63f05b5f685e6aae3274a
khushi-gtf53 <khushi.kashyap@gtftechnologies.com>
2025-09-10 13:01:42 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-    eslint: {
+  basePath: '/anantraj',
+  assetPrefix: '/anantraj/',
+
+  eslint: {
     ignoreDuringBuilds: true,
-    
+
   },
 };
 
